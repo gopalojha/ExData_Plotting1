d92bbe9fbe695f5762541a0615e410efd4624531
gopalojha <gopal.ojha@gmail.com>
2015-03-04 19:39:15 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+png("plot1.png", width = 480, height = 480)
 ## Generating Plot 3
 with(data, {
     plot(Sub_metering_1 ~ Datetime, type = "l", 
@@ -8,3 +9,4 @@ with(data, {
 
 legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
