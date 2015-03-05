f6b79ce5018fb2f2490077492ec432c47786026b
gopalojha <gopal.ojha@gmail.com>
2015-03-04 19:50:46 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,7 @@
-png("plot1.png", width = 480, height = 480)
+datafile <- file("household_power_consumption.txt")
+#data from the dates 2007-02-01 and 2007-02-02
+data <- read.table(text = grep("^[1,2]/2/2007", readLines(datafile), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+png("plot3.png", width = 480, height = 480)
 ## Generating Plot 3
 with(data, {
     plot(Sub_metering_1 ~ Datetime, type = "l", 
