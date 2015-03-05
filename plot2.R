064f13572cb7e815c06ae48fa342a4b2fa29192f
gopalojha <gopal.ojha@gmail.com>
2015-03-04 19:49:01 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,10 @@
+datafile <- file("household_power_consumption.txt")
+#data from the dates 2007-02-01 and 2007-02-02
+data <- read.table(text = grep("^[1,2]/2/2007", readLines(datafile), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
 ## Converting dates
 data$Datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
-png("plot1.png", width = 480, height = 480)
+png("plot2.png", width = 480, height = 480)
 ## Generating Plot 2
 plot(data$Datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 dev.off()
