29d133322ad4df465d214041eb53efa9ecaeef3b
gopalojha <gopal.ojha@gmail.com>
2015-03-04 19:37:51 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,7 @@
 ## Converting dates
 data$Datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
+png("plot1.png", width = 480, height = 480)
 ## Generating Plot 2
 plot(data$Datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
